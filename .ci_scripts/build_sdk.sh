58e8cddf059e9716ca50ba1f6bb43d3fa6f5f5a8
Jan Kowalewski <jkowalewski@cthings.co>
2025-08-27 01:38:12 +0200

    Fix USB default PID
    Signed-off-by: Jan Kowalewski <jkowalewski@cthings.co>

--- .ci_scripts/build_sdk.sh
+++ .ci_scripts/build_sdk.sh
@@ -4,7 +4,7 @@ echo "BUILD_TYPE: ${BUILD_TYPE}"
 
 if [ $IFACE = "usb" ]; then
   # PID (applied for targets with USB device)
-  echo "CONFIG_USB_DEVICE_PID=0x0100" >> sysbuild/mcuboot.conf
+  echo "CONFIG_USB_DEVICE_PID=0x0101" >> sysbuild/mcuboot.conf
 fi
 
 if [ $BUILD_TYPE = "firmware" ] || [ $BUILD_TYPE = "bootloader" ]; then
